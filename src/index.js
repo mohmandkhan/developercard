ef9f7b09b699b8d670c16968d21e869d3aa3d907
Majid Khan <mohmandkhan@gmail.com>
2023-12-09 23:35:02 +0100

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img className="avatar" src="photo.png" alt="Majid Khan Mohmand" />;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Majid Khan Mohmand</h1>
+      <p>
+        Senior Full Stack Web Developer and Cloud Native Tech Stacks enthusiast.
+        Majid loves to code, travel, write and take photos. He also loves to
+        cook and do blogging.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="React" color="lightblue" />
+      <Skill skill="NodeJs" color="red" />
+      <Skill skill="Html/Css" color="yellow" />
+      <Skill skill="Javascript" color="orange" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
